666a83009f936421a30cb583f837c18aed698912
cardinaldeacre <cardinaldeacre@gmail.com>
2025-11-24 14:35:19 +0700

    add new service

---
+++ backend/services/ModulesService.js
@@ -0,0 +1,25 @@
+const knex = require('../config/database');
+
+const ModulesService = {
+	getByClassId: async classId => {
+		return knex('modules').where({class_id: classId}).orderBy('order_number', 'asc');
+	},
+
+	getById: async id => {
+		return knex('modules').where({id}).first();
+	},
+
+	create: async data => {
+		const [newModule] = await knex('modules').insert(data).returning('*');
+		return newModule;
+	},
+
+	update: async (id, data) => {
+		const [updatedModule] = await knex('modules').where({id}).update(data).returning('*');
+		return updatedModule;
+	},
+
+	delete: async id => {},
+};
+
+module.exports = ModulesService;
