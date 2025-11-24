507f4c7458bf916cb150dc7bd97c884f38bc6717
cardinaldeacre <cardinaldeacre@gmail.com>
2025-11-24 14:27:48 +0700

    add express router

--- backend/make-controller.js
+++ backend/make-controller.js
@@ -20,6 +20,8 @@ const filePath = path.join(controllersDir, fileName);
 
 // template isi controller
 const template = `
+const express = require('express');
+const router = express.Router();
 router.get('/', async (req, res) => {
     try {
       return res.status(200).json();
