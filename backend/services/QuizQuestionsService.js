d52c96d3ad56a9bf9d1902c80bc6221b88fe87f1
cardinaldeacre <cardinaldeacre@gmail.com>
2025-11-25 15:50:57 +0700

    add service

---
+++ backend/services/QuizQuestionsService.js
@@ -0,0 +1,20 @@
+const knex = require('../config/database');
+
+const QuizQuestionsService = {
+  getAll: async () => {
+  },
+
+  getById: async (id) => {
+  },
+
+  create: async (data) => {
+  },
+
+  update: async (id, data) => {
+  },
+
+  delete: async (id) => {
+  }
+};
+
+module.exports = QuizQuestionsService;
