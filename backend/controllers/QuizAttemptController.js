6b48406cac8bf7f5c78e1a895b6b6865d6b9eeca
cardinaldeacre <cardinaldeacre@gmail.com>
2025-11-25 21:36:39 +0700

    add controller

---
+++ backend/controllers/QuizAttemptController.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/auth');
+const QuizAttemptService = require('../services/QuizAttemptService');
+
+router.get('/', authMiddleware, async (req, res) => {
+	const studentId = req.user.id;
+	const {quizId} = req.params;
+
+	try {
+		const history = await QuizAttemptService.getHistoryByStudent(studentId, quizId);
+		return res.status(200).json({data: history});
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({message: 'Error server'});
+	}
+});
+
+router.post('/', authMiddleware, async (req, res) => {
+	const studentId = req.user.id;
+	const {quiz_id, answer} = req.body;
+
+	if (!quiz_id || !Array.isArray(answer)) {
+		return res.status(400).json({message: 'Quiz ID & array jawaban harus diisi'});
+	}
+
+	try {
+		const attempt = await QuizAttemptService.submitAttempt(studentId, quiz_id, answer);
+		const status = await QuizAttemptService.checkPassStatus(attempt.id); // status kelslusan
+
+		return res.status(201).json({
+			message: 'Quiz berhasil di sumbit',
+			data: {
+				...attempt,
+				is_passed: status.passed,
+				min_scode: status.min_score,
+			},
+		});
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({message: 'Error server'});
+	}
+});
+
+module.exports = router;
