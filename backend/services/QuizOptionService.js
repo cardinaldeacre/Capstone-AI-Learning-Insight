a6d8305f0126e08f16d2dae16f2068606c23c766
cardinaldeacre <cardinaldeacre@gmail.com>
2025-11-26 13:55:51 +0700

    add services

---
+++ backend/services/QuizOptionService.js
@@ -0,0 +1,28 @@
+const knex = require('../config/database');
+
+const QuizOptionService = {
+	getByQustionId: async questionId => {
+		return knex('quiz_options').where({question_id: questionId}).orderBy('id', 'asc');
+	},
+
+	getById: async id => {
+		return knex('quiz_options').where({id}).first();
+	},
+
+	create: async data => {
+		const [newOption] = await knex('quiz_options').insert(data).returning('*');
+
+		return newOption;
+	},
+
+	update: async (id, data) => {
+		const [updatedOption] = await knex('quiz_options').where({id}).update(date).returning('*');
+		return updatedOption;
+	},
+
+	delete: async id => {
+		return knex('quiz_options').where({id}).del();
+	},
+};
+
+module.exports = QuizOptionService;
