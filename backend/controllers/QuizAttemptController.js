16a01c177a8b091b1e62539a97d789be5e90f762
cardinaldeacre <cardinaldeacre@gmail.com>
2025-11-26 21:11:29 +0700

    fix error handler

--- backend/controllers/QuizAttemptController.js
+++ backend/controllers/QuizAttemptController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/auth');
+const {authMiddleware} = require('../middleware/auth');
 const QuizAttemptService = require('../services/QuizAttemptService');
 
 router.get('/', authMiddleware, async (req, res) => {
