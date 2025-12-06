508ce81fd4dd69e4201a59d58b0df6494d5ef6f1
cardinaldeacre <cardinaldeacre@gmail.com>
2025-12-06 21:57:18 +0700

    initial new service

--- frontend/src/lib/api/services/quizService.js
+++ frontend/src/lib/api/services/quizService.js
@@ -1,8 +1,11 @@
 import axiosClient from "../axiosClient";
 
 const QUIZ_ENDPOINT = {
-    baseQuiz: "/quizzes"
+    baseQuiz: "/quizzes",
+    byModule: moduleId => `/quizzes/module/${moduleId}`,
+    getQuizbyId: id => `/quizzes/${id}`,
 }
+
 export const createQuiz = async (quizData) => {
     try {
         const url = QUIZ_ENDPOINT.baseQuiz;
@@ -14,3 +17,31 @@ export const createQuiz = async (quizData) => {
         throw new Error(msg);
     }
 };
+
+export const fetchQuizByModule = async (moduleId) => {
+    try {
+        if (!moduleId) throw new Error("Module ID is required");
+        const url = QUIZ_ENDPOINT.byModule(moduleId)
+        const response = await axiosClient.get(url);
+        console.log(moduleId)
+        return response.data.data
+    } catch (error) {
+        console.error('[ModuleService.byModule] API Error : ', error);
+        return [];
+    }
+}
+
+export const fetchQuizById = async (id) => {
+    try {
+        if (!id) throw new Error("ID is required");
+
+        const url = QUIZ_ENDPOINT.getQuizbyId(id);
+        const response = await axiosClient.get(url);
+        return response.data;
+    } catch (error) {
+        console.error("Error getting quiz:", error);
+        const msg = error.response?.data?.message || "Fail to get quiz";
+        throw new Error(msg);
+    }
+}
+
