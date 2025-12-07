000c6322c3f84efe1a3a9c8367e7f4950dd88855
cardinaldeacre <cardinaldeacre@gmail.com>
2025-12-07 11:10:04 +0700

    fix return data

--- backend/services/ModulesService.js
+++ backend/services/ModulesService.js
@@ -1,12 +1,21 @@
 const knex = require('../config/database');
 
 const ModulesService = {
-	getByClassId: async classId => {
-		return knex('modules').where({class_id: classId}).orderBy('order_number', 'asc');
+	getByClassId: async (classId) => {
+		const modules = await knex('modules')
+			.where('class_id', classId)
+			.orderBy('order_number', 'asc');
+
+		for (let mod of modules) {
+			const quiz = await knex('quizzes').where('module_id', mod.id).first();
+			mod.quiz = quiz || null;
+		}
+
+		return modules;
 	},
 
 	getById: async id => {
-		return knex('modules').where({id}).first();
+		return knex('modules').where({ id }).first();
 	},
 
 	create: async data => {
@@ -16,14 +25,14 @@ const ModulesService = {
 
 	update: async (moduleId, updatedData) => {
 		const [updatedModule] = await knex('modules')
-			.where({id: moduleId})
+			.where({ id: moduleId })
 			.update(updatedData)
 			.returning('*');
 		return updatedModule;
 	},
 
 	delete: async moduleId => {
-		return knex('modules').where({id: moduleId}).del();
+		return knex('modules').where({ id: moduleId }).del();
 	},
 };
 
