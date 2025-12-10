7f8ebe23999f2abfc1adb9b28e48089ff9bca73b
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-12-10 21:41:32 +0700

    [rizky] => fixed BUG

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,18 +1,24 @@
 import { Outlet } from 'react-router-dom';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 import AppSidebar from '@/components/Sidebar/AppSidebar';
-// import Header from '@/components/Header';
 import Header from '@/components/Sidebar/Header';
+import { LayoutContext } from '@/contexts/LayoutContext';
+import { useContext } from 'react';
 
 export default function MainLayout() {
+  const { isSidebarVisible } = useContext(LayoutContext);
+
+  const sidebarWidth = isSidebarVisible ? '260px' : '0px';
+  const sidebarDisplay = isSidebarVisible ? 'block' : 'none';
+
   return (
     <SidebarProvider
       style={{
-        '--sidebar-width': '260px',
+        '--sidebar-width': sidebarWidth, // <-- Terapkan lebar yang dinamis
         '--header-height': '56px'
       }}
     >
-      <AppSidebar />
+      {isSidebarVisible && <AppSidebar />}
 
       <SidebarInset>
         <Header />
