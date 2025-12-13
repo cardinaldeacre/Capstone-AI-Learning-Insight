37b47c2ee672a868bd79239be03c0ec1fcca0fa8
dienoov <alvinodien@yahoo.co.id>
2025-12-13 21:05:59 +0700

    feat: remove unused section

--- frontend/src/components/Dashboard/LearningInsight.jsx
+++ frontend/src/components/Dashboard/LearningInsight.jsx
@@ -1,6 +1,5 @@
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card.jsx";
 import {Sparkles} from "lucide-react";
-import {ScrollArea} from "@/components/ui/scroll-area.jsx";
 
 export default function LearningInsight({insight}) {
     return (
@@ -15,20 +14,9 @@ export default function LearningInsight({insight}) {
             <CardContent className="text-sm text-neutral-700 flex flex-col gap-4">
 
                 {/* Insight 1 */}
-                <p className="leading-tight">
+                <p className="leading-tight mb-4">
                     {insight}
                 </p>
-
-                {/* Scroll area rekomendasi */}
-                <ScrollArea className="min-h-[100px] rounded-md p-3 border border-neutral-200 bg-neutral-50">
-                    <p className="text-neutral-700 text-xs leading-relaxed">
-                        <span className="font-semibold text-blue-600">Rekomendasi AI:</span><br/><br/>
-                        • Lanjutkan modul: <span className="text-blue-500">State Management</span><br/>
-                        • Fokus pada topik: <span className="text-blue-500">Async Operations</span><br/>
-                        • Durasi belajar idealmu: <span className="text-blue-500">20–30 menit</span> per sesi<br/>
-                        • Kurangi pengulangan berlebih di modul 3 — coba metode ringkasan.
-                    </p>
-                </ScrollArea>
 
             </CardContent>
         </Card>
